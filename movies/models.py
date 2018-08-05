e7887ce9a2b9d8deee29cf3ab69b03ddb16765d6
Reyuu <rey444xd3@gmail.com>
2018-08-05 16:06:16 +0000

    tests and database fixes

--- movies/models.py
+++ movies/models.py
@@ -7,8 +7,8 @@ class Movie(models.Model):
     Year = models.IntegerField() # only year
     Rated = models.CharField(max_length=10, blank=True)
     #Released = models.CharField(max_length=11, blank=True) #date fix 
-    Released = models.DateField(blank=True) #date fix 
-    Runtime = models.IntegerField(blank=True) # minutes
+    Released = models.DateField(blank=True, null=True) #date fix 
+    Runtime = models.IntegerField(blank=True, null=True) # minutes
     Genre = models.CharField(max_length=256, blank=True) # separated by comma
     Director = models.CharField(max_length=128, blank=True)
     Writer = models.TextField(blank=True)
@@ -19,13 +19,13 @@ class Movie(models.Model):
     Awards = models.TextField(blank=True)
     Poster = models.URLField(blank=True)
     Ratings = models.TextField(blank=True) # source:value;source:value
-    Metascore = models.IntegerField(blank=True)
-    imdbRating = models.FloatField(blank=True)
-    imdbVotes = models.IntegerField(blank=True) # replace commas with nothing
+    Metascore = models.IntegerField(blank=True, null=True)
+    imdbRating = models.FloatField(blank=True, null=True)
+    imdbVotes = models.IntegerField(blank=True, null=True) # replace commas with nothing
     imdbID = models.CharField(max_length=32, blank=True)
     Type = models.CharField(max_length=16)
     #DVD = models.CharField(max_length=11, blank=True) #date fix
-    DVD = models.DateField(blank=True) #date fix
+    DVD = models.DateField(blank=True, null=True) #date fix
     BoxOffice = models.CharField(max_length=32, blank=True)
     Production = models.CharField(max_length=64)
     Website = models.URLField(blank=True)
--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory, Client
+from .views import movie, comment
+from .models import Movie, Comment
+import json
+import requests
 
 # Create your tests here.
+
+class MovieAndCommentTest(TestCase):
+    def setUp(self):
+        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
+        self.movie = Movie(id=0, Title="Gone with the Wind", Year=1992, Type="movie", Response="True")
+        self.comment = Comment(movie_id=0, comment_body="Some comment very nice")
+        self.movie.save()
+        self.comment.save()
+
+    def test_movie_post(self):
+        title = "Guardians of the Galaxy"
+        response = self.client.post("/movies", {"title": title})
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.content)
+        self.assertEqual(j["Title"], title)
+
+    def test_movie_get(self):
+        response = self.client.get("/movies")
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.content)
+        self.assertEqual(len(j), Movie.objects.count())
+    
+    def test_movie_get_title(self):
+        title = "Gone with the Wind"
+        response = self.client.get("/movies", {"title": title})
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.content)
+        self.assertEqual(j[0]["Title"], title)
+
+    def test_comment_post(self):
+        data = {"movie_id": 0, "comment": "Very nice!"}
+        response = self.client.post("/comments", data)
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.content)
+        self.assertEqual(j["movie_id"], data["movie_id"])
+        self.assertEqual(j["comment_body"], data["comment"])
+
+    def test_comment_get(self):
+        response = self.client.get("/comments")
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.content)
+        self.assertEqual(len(j), Comment.objects.count())
+    
+    def test_comment_get_by_movie_id(self):
+        data = {"movie_id": 0}
+        response = self.client.get("/comments", )
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.content)
+        self.assertEqual(len(j), Comment.objects.filter(movie_id=data["movie_id"]).count())
